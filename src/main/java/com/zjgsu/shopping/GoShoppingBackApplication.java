85d71b82239edcd479d013d0458271f82b453512
徐一苇 <xuyiwei@xuyiweideMBP.lan>
2021-09-14 23:16:46 +0800

    Initial commit

---
+++ src/main/java/com/zjgsu/shopping/GoShoppingBackApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoShoppingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoShoppingBackApplication.class, args);
+    }
+
+}
