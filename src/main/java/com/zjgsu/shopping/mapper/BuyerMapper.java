ae43fa2df500661901f707a428c5234e76456741
xuyiwei0812 <29541317@qq.com>
2021-09-16 20:13:55 +0800

    BuyerMapper

--- src/main/java/com/zjgsu/shopping/mapper/BuyerMapper.java
+++ src/main/java/com/zjgsu/shopping/mapper/BuyerMapper.java
@@ -1,5 +1,27 @@
 package com.zjgsu.shopping.mapper;
 
+import com.zjgsu.shopping.pojo.Buyer;
+import org.apache.ibatis.annotations.*;
+
 //存放买家的信息
-public class BuyerMapper {
+@Mapper
+public interface BuyerMapper {
+    /**
+     * 提出一个购买意向
+     *
+     * @param buyer 买家信息
+     * @return 提出失败返回-1
+     */
+    @Insert("insert into intentionbuyer (buyerId,name,location,phone) values (#{buyer.buyerId},#{buyer.name},#{buyer.location},#{buyer.phone})")
+    Boolean raiseIntention(@Param("buyer") Buyer buyer);
+
+    /**
+     * 撤销一个购买意向
+     *
+     * @param buyer
+     * @return 撤销失败 返回-1
+     *
+     */
+    @Delete("delete from intentionbuyer where buyerId=#{buyer.buyerId}")
+    Boolean cancelIntention(@Param("buyer") Buyer buyer);
 }
