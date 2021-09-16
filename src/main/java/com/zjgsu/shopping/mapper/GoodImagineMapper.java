ebafd6fe88cebe3d768f0e54184890f06207000b
K-desperate <2050284@qq.com>
2021-09-16 20:24:51 +0800

    goodimagine

--- src/main/java/com/zjgsu/shopping/mapper/GoodImagineMapper.java
+++ src/main/java/com/zjgsu/shopping/mapper/GoodImagineMapper.java
@@ -1,5 +1,38 @@
 package com.zjgsu.shopping.mapper;
 
-//存放商品图片
-public class GoodImagineMapper {
+import com.zjgsu.shopping.pojo.GoodImagine;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface GoodImagineMapper {
+    /**
+     * 添加一张图片
+     *
+     * @param goodImagine 图片商品
+     * @return 失败返回0
+     */
+    @Options(useGeneratedKeys = true, keyProperty = "imagineId", keyColumn = "imagineId")
+    @Insert("insert into goodimagine(goodId,imagine) values (#{goodImagine.goodId}," +
+            "#{goodImagine.imagine})")
+    Boolean addImagine(@Param("goodImagine")GoodImagine goodImagine);
+
+    /**
+     * 删除一张图片
+     *
+     * @param imagineId 图片编号
+     * @return 失败返回0
+     */
+    @Delete("delete from goodimagine where imagineId =#{imagineId}")
+    Boolean deleteImagine(@Param("imagineId") int imagineId);
+
+    /**
+     * 查询某商品的图片
+     *
+     * @param goodId 商品编号
+     * @return List<GoodImagine>
+     */
+    @Select("select * from goodimagine where goodId =#{goodId}")
+    List<GoodImagine> getImagine(@Param("goodId") int goodId);
 }
