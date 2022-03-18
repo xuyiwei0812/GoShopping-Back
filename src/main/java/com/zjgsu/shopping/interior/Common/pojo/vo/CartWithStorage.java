2ce29dcf7644d89baf16f0537677c22d64054913
xuyiwei0812 <29541317@qq.com>
2022-03-18 14:01:35 +0800

    。

--- src/main/java/com/zjgsu/shopping/interior/Common/pojo/vo/CartWithStorage.java
+++ src/main/java/com/zjgsu/shopping/interior/Common/pojo/vo/CartWithStorage.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CartWithStorage{
+public class CartWithStorage {
     CartWithImg cartWithImg;
     Integer storage;
 }
